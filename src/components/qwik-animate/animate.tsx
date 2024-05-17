607ab5c27b7403786ce2a356dfe85723c356cd4d
HomeJa <revanamike620@gmail.com>
2024-05-17 08:11:10 +0300

    update🙂

--- src/components/qwik-animate/animate.tsx
+++ src/components/qwik-animate/animate.tsx
@@ -1,7 +1,7 @@
 import { component$, Slot, useStyles$, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import animateStyles from './animate.css?inline';
 
-export const Animate = component$(({ optionAttribute, animationRepeat = 'true', class: classProp }: { optionAttribute: string, animationRepeat?: string, class?: string }) => {
+export const Animate = component$(({ optionAttribute, class: classProp }: { optionAttribute: string, class?: string }) => {
   useStyles$(animateStyles);
 
   const store = useStore<{ elements: Record<string, boolean> }>({ elements: {} });
@@ -14,14 +14,13 @@ export const Animate = component$(({ optionAttribute, animationRepeat = 'true', 
         entries.forEach(entry => {
           const elementId = entry.target.getAttribute('option-attribute');
           if (elementId) {
-            const shouldRepeat = entry.target.getAttribute('animation-repeat') === 'true';
             if (entry.isIntersecting) {
               if (!store.elements[elementId]) {
                 store.elements[elementId] = true;
                 console.log('Component is in view:', elementId);
               }
             } else {
-              if (store.elements[elementId] && shouldRepeat) {
+              if (store.elements[elementId]) {
                 store.elements[elementId] = false;
                 console.log('Component is not in view:', elementId);
               }
@@ -42,7 +41,6 @@ export const Animate = component$(({ optionAttribute, animationRepeat = 'true', 
     <div
       class={classProp}
       option-attribute={optionAttribute}
-      animation-repeat={animationRepeat}
       qwik-animate={store.elements[optionAttribute] ? optionAttribute : undefined}
     >
       <Slot />
